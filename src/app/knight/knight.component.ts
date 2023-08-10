100faccf39ebb3182abe53a6ddd4a93d0a5ef6c4
pawelzdrojewski <zdrojewski_paw@wp.pl>
2023-08-10 09:54:16 +0200

    rebuilding pipe

--- src/app/knight/knight.component.ts
+++ src/app/knight/knight.component.ts
@@ -1,6 +1,6 @@
 import { LocationStrategy } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
-import { interval, last, take } from 'rxjs';
+import { Observable, delay, interval, last, map, of, take } from 'rxjs';
 import { PointXY } from '../point-xy';
 
 @Component({
@@ -41,6 +41,7 @@ export class KnightComponent implements OnInit {
  tablica: number[][] = [[],[],[],[],[],[],[],[]];
 
  stos: stos[] =[]
+ wynik = of(this.stos)
  flat_matrix: number[] = [];
 
  kroki: krok[] = [];
@@ -63,13 +64,13 @@ ruch: ruch[] =[{ X: (this.x  ), Y: (this.y  ) }, //0
 
   trasa(x: number, y: number, n: number, i: number){
     this.matrix[x][y] = n;
-    // this.kroki.push({X: x, Y: y});
+    this.kroki.push({X: x, Y: y});
    // console.log(`n:${this.matrix[x][y]} x:${x} y:${y}`);
    //this.i = 1
     if(x-2>=0 && x-2<=7 && y+1>=0 && y+1<=7 && !(this.matrix[x-2][y+1] > 0)){ //PGG
         i = 1
         x = x-2; y = y+1; n++;
-        // this.kroki.push({X: x, Y: y});
+        this.kroki.push({X: x, Y: y});
         this.matrix[x][y] = n;
         // console.log(`1 n:${this.matrix[x][y]} x:${x} y:${y}`);
         this.stos.push({x, y, n, i})
@@ -80,7 +81,7 @@ ruch: ruch[] =[{ X: (this.x  ), Y: (this.y  ) }, //0
     if (x-1>=0 && x-1<=7 && y+2>=0 && y+2<=7 && !(this.matrix[x-1][y+2] > 0)) {//PPG
       i = 2
         x = x-1; y = y+2; n++;
-        // this.kroki.push({X: x, Y: y});
+        this.kroki.push({X: x, Y: y});
          this.matrix[x][y] = n;
         //  console.log(`2 n:${this.matrix[x][y]} x:${x} y:${y}`);
          this.stos.push({x, y, n, i})
@@ -91,7 +92,7 @@ ruch: ruch[] =[{ X: (this.x  ), Y: (this.y  ) }, //0
     if (x+1>=0 && x+1<=7 && y+2>=0 && y+2<=7 && !(this.matrix[x+1][y+2] > 0)) { //PPD
       i = 3
         x = x+1; y = y+2; n++;
-        // this.kroki.push({X: x, Y: y});
+        this.kroki.push({X: x, Y: y});
         this.matrix[x][y] = n;
         // console.log(`3 n:${this.matrix[x][y]} x:${x} y:${y}`);
         this.stos.push({x, y, n, i})
@@ -103,7 +104,7 @@ ruch: ruch[] =[{ X: (this.x  ), Y: (this.y  ) }, //0
       i = 4
         x = x+2; y = y+1; n++;
         this.matrix[x][y] = n;
-        // this.kroki.push({X: x, Y: y});
+        this.kroki.push({X: x, Y: y});
         // console.log(`4 n:${this.matrix[x][y]} x:${x} y:${y}`);
         this.stos.push({x, y, n, i})
          console.log(this.stos[n])
@@ -113,7 +114,7 @@ ruch: ruch[] =[{ X: (this.x  ), Y: (this.y  ) }, //0
     if (x+2>=0 && x+2<=7 && y-1>=0 && y-1<=7 && !(this.matrix[x+2][y-1] > 0)) {//LDD
         i = 5
         x = x+2; y = y-1; n++;
-        // this.kroki.push({X: x, Y: y});
+        this.kroki.push({X: x, Y: y});
         this.matrix[x][y] = n;
         // console.log(`5 n:${this.matrix[x][y]} x:${x} y:${y}`);
         this.stos.push({x, y, n, i})
@@ -124,7 +125,7 @@ ruch: ruch[] =[{ X: (this.x  ), Y: (this.y  ) }, //0
     if (x+1>=0 && x+1<=7 && y-2>=0 && y-2<=7 && !(this.matrix[x+1][y-2] > 0)) { //LLD
       i = 6
       x = x+1; y = y-2; n++;
-      // this.kroki.push({X: x, Y: y});
+      this.kroki.push({X: x, Y: y});
         this.matrix[x][y] = n;
         // console.log(`6 n:${this.matrix[x][y]} x:${x} y:${y}`);
         this.stos.push({x, y, n, i})
@@ -135,7 +136,7 @@ ruch: ruch[] =[{ X: (this.x  ), Y: (this.y  ) }, //0
     if (x-1>=0 && x-1<=7 && y-2>=0 && y-2<=7 && !(this.matrix[x-1][y-2] > 0)) { //LLG
       i = 7
         x = x-1; y = y-2; n++;
-        // this.kroki.push({X: x, Y: y});
+        this.kroki.push({X: x, Y: y});
         this.matrix[x][y] = n;
         // console.log(`7 n:${this.matrix[x][y]} x:${x} y:${y}`);
         this.stos.push({x, y, n, i})
@@ -146,7 +147,7 @@ ruch: ruch[] =[{ X: (this.x  ), Y: (this.y  ) }, //0
     if (x-2>=0 && x-2<=7 && y-1>=0 && y-1<=7 && !(this.matrix[x-2][y-1] > 0)) { //LGG
         i = 8
         x= x-2; y=y-1; n++;
-        // this.kroki.push({X: x, Y: y});
+        this.kroki.push({X: x, Y: y});
         this.matrix[x][y] = n;
         // console.log(`8 n:${this.matrix[x][y]} x:${x} y:${y}`);
         this.stos.push({x, y, n, i})
@@ -155,10 +156,11 @@ ruch: ruch[] =[{ X: (this.x  ), Y: (this.y  ) }, //0
     }
      else {
       console.log("Konic drogi!");
-      //this.stos.splice(-1);
-      this.stos[n].n = 0
-      this.trasa(this.stos[n-1].x, this.stos[n-1].y, this.stos[n-1].n, this.stos[n-1].i)
-     // return
+    //   this.stos.splice(-1);
+    //  // this.stos[n].n = 0
+    //   this.kroki.splice(-1)
+    //   this.trasa(this.stos[n-1].x, this.stos[n-1].y, this.stos[n-1].n, this.stos[n-1].i)
+    //   return
      }
 
   }   
@@ -168,9 +170,10 @@ ruch: ruch[] =[{ X: (this.x  ), Y: (this.y  ) }, //0
   }
  
   krok(){
-    const obj = interval(1000);
-    const zakres = obj.pipe(take(128));
-     zakres.subscribe((a) => {this.tablica[this.kroki[a].X][this.kroki[a].Y] = this.matrix[this.kroki[a].X][this.kroki[a].Y]; this.a = a;});
+    //const obj = interval(1000);
+   // const zakres = obj.pipe(take(57));
+     //zakres.subscribe((a) => {this.tablica[this.kroki[a].X][this.kroki[a].Y] = this.matrix[this.kroki[a].X][this.kroki[a].Y]; this.a = a;});
+     this.wynik.pipe(delay(5000), map(a=>{console.log(a)})).subscribe()
   }
 
   zeruj(m: number[][]){
